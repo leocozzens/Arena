60ee32fc23f2438ccf66cc23b40a3c8bc49b43a8
Leo Cozzens <leo@narthexnetworks.com>
2023-12-03 11:05:17 -0600

    Completed first challenge and project setup
    Completed AoC 2023 challenge 1
    Completed build system
    Completed challenge unit container setup

---
+++ AdventOfCode2023/src/1.c
@@ -0,0 +1,86 @@
+// C standard headers
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+// Local headers
+#include <container.h>
+
+#define EXIT_SUCCESS                0
+#define EXIT_FAILURE                1
+#define DIGITS                      10
+#define VAL_SIZE                    3
+#define EMPTY_VALS                  "NN"
+#define EMPTY_VAL                   'N'
+#define WORD_OVERLAP                2
+#define VALUE_TYPE                  "CALIBRATION VALUE"
+#define S_LEN(_x)                   (sizeof(_x) - 1)
+#define DIGIT_ENTRY(_word, _digit)  { _word, S_LEN(_word), _digit }
+
+typedef struct {
+    char *word;
+    int wordLen;
+    char digit;
+} Digit;
+
+
+Digit digitTable[] = {
+    DIGIT_ENTRY("zero", '0'),
+    DIGIT_ENTRY("one", '1'),
+    DIGIT_ENTRY("two", '2'),
+    DIGIT_ENTRY("three", '3'),
+    DIGIT_ENTRY("four", '4'),
+    DIGIT_ENTRY("five", '5'),
+    DIGIT_ENTRY("six", '6'),
+    DIGIT_ENTRY("seven", '7'),
+    DIGIT_ENTRY("eight", '8'),
+    DIGIT_ENTRY("nine", '9')
+};
+
+static void reset_buff(char *buff, int *output);
+static int convert_num(char *str);
+static char match_num(char *target, long *iterator);
+
+ReturnData eval(char *data, long fSize) {
+    ReturnData outData = { VALUE_TYPE, NULL, 0, EXIT_SUCCESS };
+    char val[VAL_SIZE] = EMPTY_VALS;
+
+    for(long i = 0; i < fSize; i++) {
+        if(data[i] == '\n' || data[i] == '\r') {
+            reset_buff(val, &outData.output);
+            continue;
+        }
+        char result = match_num(data + i, &i);
+        if(result != '\0') {
+            if(val[0] == EMPTY_VAL) val[0] = result;
+            val[1] = result;
+        }
+    }
+    reset_buff(val, &outData.output);
+    return outData;
+}
+
+static void reset_buff(char *buff, int *output) {
+    if(buff[0] != EMPTY_VAL) {
+        *output += convert_num(buff);
+        memcpy(buff, EMPTY_VALS, VAL_SIZE);
+    }
+}
+
+static int convert_num(char *str) {
+    int ret = 0;
+    while(*str != '\0')
+        ret = ret * 10 + *str++ - '0';
+    return ret;
+}
+
+static char match_num(char *target, long *iterator) {
+    for(int i = 0; i < DIGITS; i++) {
+        if(*target == digitTable[i].digit)
+            return digitTable[i].digit;
+        if(strncmp(target, digitTable[i].word, digitTable[i].wordLen) == 0) {
+            *iterator += digitTable[i].wordLen - WORD_OVERLAP;
+            return digitTable[i].digit;
+        }
+    }
+    return '\0';
+}
---
+++ AdventOfCode2023/src/container.c
@@ -0,0 +1,104 @@
+// C standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+// Local headers
+#include <container.h>
+
+#define GENERAL_ERR     "ERROR READING DATA - %s"
+#define EVAL_ERR        "EVALUATION ERROR - %s\nERROR CODE - %d\n"
+#define UNLISTED_ERR    "UNLISTED ERROR"
+#define UNLISTED_VAL    "VALUE"
+#define USAGE           "%s [DATA PATH]"
+#define DEFAULT_NAME    "challenge.exe"
+#define READ_MODE       "rb"
+
+#define SERVICE_CHECK(_cond)    if(_cond) { \
+                                    fprintf(stderr, GENERAL_ERR, strerror(errno)); \
+                                    return EXIT_FAILURE; \
+                                }
+
+ReturnData EMPTY_DATA = { NULL, NULL, 0, EXIT_SUCCESS };
+
+ReturnData eval(char *data, long fSize);
+static char *extract_name(char *filePath);
+static char *read_file(FILE *target, long *fSize);
+static void print_char_len(char fill, int refLen);
+static int determine_digits(int target);
+static void print_output(const char *type, int output);
+
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        printf(USAGE, (argv[0] == NULL) ? DEFAULT_NAME : extract_name(argv[0]));
+        return EXIT_FAILURE;
+    }
+    FILE *dataFile = fopen(argv[1], READ_MODE);
+    SERVICE_CHECK(dataFile == NULL);
+
+    long fSize;
+    char *data = read_file(dataFile, &fSize);
+    fclose(dataFile);
+    SERVICE_CHECK(data == NULL);
+
+    ReturnData evalData = eval(data, fSize);
+    free(data);
+
+    if(evalData.retCode == EXIT_SUCCESS)
+        print_output((evalData.type == NULL) ? UNLISTED_VAL : evalData.type, evalData.output);
+    else
+        fprintf(stderr, EVAL_ERR, (evalData.errVal == NULL) ? UNLISTED_ERR : evalData.errVal, evalData.retCode);
+    return evalData.retCode;
+}
+
+static char *extract_name(char *filePath) {
+    char *lastSlash = strrchr(filePath, '/');
+    if(lastSlash == NULL) lastSlash = strrchr(filePath, '\\');
+    return (lastSlash == NULL) ? filePath : lastSlash + 1;
+}
+
+static char *read_file(FILE *target, long *fSize) {
+    if(target == NULL) return NULL;
+    if(fseek(target, 0, SEEK_END) == -1) return NULL;
+    *fSize = ftell(target);
+    if(*fSize == -1) return NULL;
+    if(fseek(target, 0, SEEK_SET) == -1) return NULL;
+
+    char *store = malloc(*fSize + 1);
+    if(store == NULL) return NULL;
+    size_t ret = fread(store, sizeof(char), *fSize, target);
+    if(ret < *fSize) {
+        free(store);
+        errno = EIO;
+        return NULL;
+    }
+    store[*fSize] = '\0';
+    return store;
+}
+
+static void print_char_len(char fill, int refLen) {
+    for(int i = 0; i < refLen; i++)
+        printf("%c", fill);
+}
+
+static int determine_digits(int target) {
+    int digits = 0;
+    while(target != 0) {
+        target /= 10;
+        digits++;
+    }
+    return digits;
+}
+
+static void print_output(const char *type, int output) {
+    int typeLen = strlen(type);
+    print_char_len('-', typeLen);
+    printf("\n%s\n", type);
+
+    print_char_len('-', typeLen);
+    printf("\n");
+    print_char_len(' ', typeLen / 2 - determine_digits(output) / 2);
+    printf("%d\n", output);
+    print_char_len(' ', typeLen);
+    printf("\n");
+}
