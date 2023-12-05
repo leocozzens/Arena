d5f080182062dec868edc69c283e27683857a306
Leo Cozzens <leo@narthexnetworks.com>
2023-12-04 20:21:04 -0600

    Initialized challenge 3

---
+++ AdventOfCode2023/src/challenge_3.c
@@ -0,0 +1,19 @@
+// C standard headers
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+// Local headers
+#include <container.h>
+#include <utils.h>
+
+#define VALUE_TYPE          "SUM OF ENGINE PART NUMBERS"
+
+ReturnData eval(char *data, long fSize) {
+    set_linefeed(data);
+    char *lineFeed;
+    while((lineFeed = get_linefeed()) != NULL)
+        printf("%s\n", lineFeed);
+
+    ReturnData outData = { VALUE_TYPE, strerror(errno), 0, errno };
+    return outData;
+}
